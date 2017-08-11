cfd769cad329178cd245611577b003ce5b8f7147
Manoj Singh Negi <me@manojsinghnegi.com>
2017-08-11 06:29:00 +0530

    added opendialog

--- renderer.js
+++ renderer.js
@@ -2,14 +2,28 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-var convertButton = document.querySelector('#convert');
+const {dialog} = require('electron').remote
+
+const convertButton = document.querySelector('#convert');
+const chooseButton = document.querySelector('#choose');
 
 init();
 
 function init () {
 	convertButton.addEventListener('click', changeState);
+	chooseButton.addEventListener('click', openDialog)
 }
 
 function changeState () {
 	convertButton.innerHTML = '<div class="loader">Loading...</div>';
+}
+
+function openDialog () {
+	dialog.showOpenDialog({
+		properties: ['openFile', 'openDirectory']
+	}, (filePaths) => {
+
+		console.log(filePaths);
+
+	})
 }
